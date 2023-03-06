0c97ddb6810d5d2e810e698670c0a141aa92b58e
PyeonMinjun <alswns9810@gmail.com>
2023-03-06 21:12:55 +0900

    테스트작성 및 Delete, Update, Service

--- src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -3,6 +3,8 @@ package com.example.gccoffee.repository;
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
 //import jdk.jfr.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -39,26 +41,58 @@ public class ProductJdbcRepository implements ProductRepository {
 
     @Override
     public Product update(Product product) {
-        return null;
+         var update= jdbcTemplate.update(
+                "UPDATE products SET product_name = :productName, category = :category , price = :price, description = :description, created_at= :createdAt, updated_at= :updatedAt" +
+                       " WHERE product_id = UUID_TO_BIN(:productId)",
+                toParamMap(product)
+        );
+         if(update != 1) {
+             throw new RuntimeException("Noting was update");
+         }
+    return product;
+
     }
 
     @Override
     public Optional<Product> findById(UUID productId) {
-        return Optional.empty();
+        try{
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UUID_TO_BIN(:productId) ",
+                            Collections.singletonMap("productId", productId.toString().getBytes()), productRowMapper)
+            );
+
+        } catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Product> findByName(String productName) {
-        return Optional.empty();
+        try{
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                            Collections.singletonMap("productName", productName), productRowMapper)
+            );
+
+        } catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Product> findByCategory(Category category) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category", category.toString()),
+                productRowMapper
+        );
     }
-
     @Override
     public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM products", Collections.emptyMap());
+    }
+
+    public void clearStore()        {
 
     }
 
--- src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
+++ src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -2,36 +2,85 @@ package com.example.gccoffee.repository;
 
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 
-
+//@Transactional
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class ProductJdbcRepositoryTest {
 
     @Autowired
     ProductRepository repository;
 
+
     private final Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
     @Test
+    @Order(1)
     @DisplayName("상품을 추가할 수 있다.")
     void testInsert() {
         repository.insert(newProduct);
-        List<Product> all = repository.findAll();
+        var all = repository.findAll();
         assertThat(all.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("상품을 이름으로 조회할 수 있다.")
+    void testFindByName() {
+        var product = repository.findByName(newProduct.getProductName());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품을 아이디로 조회할 수 있다.")
+    void testFindById() {
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+
+    @Test
+    @Order(4)
+    @DisplayName("상품들을 카테고리으로 조회할 수 있다.")
+    void testFindByCategory () {
+        var product = repository.findByCategory(Category.COFFEE_BEAN_PACKAGE);
+        assertThat(product.isEmpty(), is(false));
+        }
+    @Test
+    @Order(5)
+    @DisplayName("상품을 수정할 수 있다.")
+    void testUpdate(){
+        newProduct.setProductName("updated-product");
+        repository.update(newProduct);
+
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+        assertThat(product.get(), samePropertyValuesAs(newProduct));
+    }
+    @Test
+    @Order(6)
+    @DisplayName("상품을 전체 삭제한다.")
+    void testDeleteAll(){
+        repository.deleteAll();
+        var all = repository.findAll();
+        assertThat(all.isEmpty(), is(true));
     }
 
 
